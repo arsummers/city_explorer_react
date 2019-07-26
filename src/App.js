bae3b926df7da9e1b5b6f6c16ad44dce03cbd9f1
arsummers <aliyasummers1@gmail.com>
2019-07-25 21:08:22 -0700

    scss imported

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, {Component}  from 'react';
 import Header from './components/header';
 import Main from './components/main';
 import './App.css';
-
-
+import './scss/app.scss'
 
 class App extends Component {
   render(){
